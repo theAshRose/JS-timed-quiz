50c0f91dc7a7b310fdb3752d831bb2265846183a
theDomconrad <them.and.us.2013@gmail.com>
2022-09-30 20:03:50 -0700

    added base files, pseudocode

---
+++ assets/js/outline.js
@@ -0,0 +1,21 @@
+
+
+//variables for body elements
+
+//object for quiz questions, asnwers, and one array for correct answers
+
+//need iteration loop to randomize question order, answer order
+
+//switches, one for start, one for question input, 
+
+//need appending for buttons in lists
+
+//one for scoreboard and one for return to home(from scoreboard)
+
+//need variable for score to save
+
+//need json.parse to parse up scores at end, with a button to clear scores
+
+//parent object with variables and arrays for questions+answers
+
+//need element.hidden for each portion of page
