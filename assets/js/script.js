755c462a8cef3b86a973b9b5526399e60b67db19
theDomconrad <them.and.us.2013@gmail.com>
2022-10-01 15:27:53 -0700

    fixed bugs, wrote functions to hide elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,105 @@
-//button variables
-var startButton = document.querySelector("#start-button");
-var highScoreButton = document.getElementById("#score-page-button");
-var aButton = document.getElementsByClassName(".a-button");
-var aList = document.getElementById("#a-list");
-var homeButton = document.getElementById("#home-button");
-var clearScore = document.getElementById("#clear-button");
+//home page
 
-//page elements
-var userInitials = document.getElementById("#user-initials");
-var scoreList = document.querySelector(".score-list");
-var timer = document.getElementById("#timer");
-//div variables "pages"
-var homePage = document.querySelector("#Home");
+var startButton = document.querySelector("#start-button");
+var scorePageButton = document.querySelector("#score-page-button");
+
+var homePage = document.querySelector("#home");
+
+//quiz page
+
+var aButton = document.getElementsByClassName(".a-button");
+
+var question = document.getElementById("#question");
+var aList = document.getElementById("#a-list");
+
 var quizPage = document.getElementById("#quiz");
 
+var timer = document.getElementById("#timer");
+
+//score page
+
+var homeButton = document.querySelector("#home-button");
+var clearScore = document.querySelector("#clear-button");
+
+var scoreList = document.querySelector(".score-list");
 var scorePage = document.querySelector("#score-page");
-var scoreInput = document.querySelector(".score-input");
+var scoreData = document.querySelector(".score-data");
+
+//initials input page
+var initialsButton = document.querySelector("#user-initials-button");
+
+var initialsInput = document.getElementById("#user-initials");
+
+var initialScore = document.getElementById("#user-score");
+var initialsPage = document.getElementById("#post-game-input");
+
 
 //event listeners
 startButton.addEventListener("click", startQuiz);
-scorePage.addEventListener("click", scorePageGo);
+scorePageButton.addEventListener("click", scorePageGo);
+clearScore.addEventListener("click", clearScores);
+homeButton.addEventListener("click", pageLoad);
 
 
-clearScore.addEventListener("click", scorePageGo);
-
-homeButton.addEventListener("click", scorePageGo);
+pageLoad();
 
 // default hidden sections:
+// document.getElementById("#score-page").hidden = true;
+// document.getElementById("#home").hidden = false;
+// 
 
-
-document.getElementById("#score-page").hidden = true;
-document.getElementById("#quiz").hidden = true;
-document.getElementById("#home").hidden = false;
-document.getElementById("#home-button").hidden = true;
-
-
-
-function clearScores() {
+function pageLoad() {
+    document.querySelector("#home").hidden = false;
+    document.querySelector("#quiz").hidden = true;
+    document.querySelector("#post-game-input").hidden = true;
+    document.querySelector("#score-page").hidden = true;
+    document.querySelector("#home-button").hidden = true;
+    document.querySelector("#score-page-button").hidden = false;
 }
 
+function clearScores() {
+};
+
 function scorePageGo() {
-window.location.reload();
-return;
+    document.querySelector("#home").hidden = true;
+    document.querySelector("#quiz").hidden = true;
+    document.querySelector("#post-game-input").hidden = true;
+    document.querySelector("#score-page").hidden = false;
+    document.querySelector("#home-button").hidden = false;
+    document.querySelector("#score-page-button").hidden = true;
+};
+
+var questionArray = [
+    {
+        questionText: "Fill in blank: JavaScript is a ______ language", 
+        answers: ["body", "coffee language", "dutch", "programming"],
+        correctAnswer: "programming",
+    },
+    {
+        questionText: "When was Javascript created?", 
+        answers: ["1969", "1995", "12077", "2012"],
+        correctAnswer: "1995",
+    }
+];
+
+var currentQuestion = [];
+populateQuestion();
+function populateQuestion() {
+    currentQuestion = questionArray[0];
+    console.log(currentQuestion);
+
+  //questionIndex++;
+
 }
 
 function startQuiz() {
-startButton.textContent = "START MEEE";
-return;
+    document.querySelector("#home").hidden = true;
+    document.querySelector("#quiz").hidden = false;
+    document.querySelector("#post-game-input").hidden = true;
+    document.querySelector("#score-page").hidden = true;
+    document.querySelector("#home-button").hidden = true;
+    document.querySelector("#score-page-button").hidden = false;
+
+    
 }
 
-var question = document.getElementById("#question")
-var questionArray = [
-    {"This is a question?":
-    ["answer1", "answer2", "answer3", "answer 4"
-    ["correct answer"]]},
-
-    {"This is a question?":
-    ["answer1", "answer2", "answer3", "answer 4"
-    ["correct answer"]]},
-
-    {"This is a question?":
-    ["answer1", "answer2", "answer3", "answer 4"
-    ["correct answer"]]},
-
-    {"This is a question?":
-    ["answer1", "answer2", "answer3", "answer 4"
-    ["correct answer"]]},
-
-    {"This is a question?":
-    ["answer1", "answer2", "answer3", "answer 4"
-    ["correct answer"]]},
-
-    {"This is a question?":
-    ["answer1", "answer2", "answer3", "answer 4"
-    ["correct answer"]]}
-];
