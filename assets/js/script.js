d5f1f696714b8c9865a0e078422f04ddbe1a5fdf
theDomconrad <them.and.us.2013@gmail.com>
2022-10-02 00:18:31 -0700

    added timer, questions are responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,18 @@ var homePage = document.querySelector("#home");
 
 //quiz page
 
-var aButton = document.getElementsByClassName(".a-button");
+var aButton1 = document.querySelector("#aButton1");
+var aButton2 = document.querySelector("#aButton2");
+var aButton3 = document.querySelector("#aButton3");
+var aButton4 = document.querySelector("#aButton4");
 
-var question = document.getElementById("#question");
-var aList = document.getElementById("#a-list");
 
-var quizPage = document.getElementById("#quiz");
+var question = document.getElementById("question");
+var aList = document.querySelector("#a-list");
 
-var timer = document.getElementById("#timer");
+var quizPage = document.querySelector("#quiz");
+
+var timer = document.querySelector("#timer");
 
 //score page
 
@@ -28,10 +32,12 @@ var scoreData = document.querySelector(".score-data");
 //initials input page
 var initialsButton = document.querySelector("#user-initials-button");
 
-var initialsInput = document.getElementById("#user-initials");
+var initialsInput = document.querySelector("#user-initials");
 
-var initialScore = document.getElementById("#user-score");
-var initialsPage = document.getElementById("#post-game-input");
+var initialScore = document.querySelector("#user-score");
+var initialsPage = document.querySelector("#post-game-input");
+
+
 
 
 //event listeners
@@ -39,35 +45,11 @@ startButton.addEventListener("click", startQuiz);
 scorePageButton.addEventListener("click", scorePageGo);
 clearScore.addEventListener("click", clearScores);
 homeButton.addEventListener("click", pageLoad);
+initialsButton.addEventListener("click", scorePageGo);
 
 
-pageLoad();
 
-// default hidden sections:
-// document.getElementById("#score-page").hidden = true;
-// document.getElementById("#home").hidden = false;
-// 
 
-function pageLoad() {
-    document.querySelector("#home").hidden = false;
-    document.querySelector("#quiz").hidden = true;
-    document.querySelector("#post-game-input").hidden = true;
-    document.querySelector("#score-page").hidden = true;
-    document.querySelector("#home-button").hidden = true;
-    document.querySelector("#score-page-button").hidden = false;
-}
-
-function clearScores() {
-};
-
-function scorePageGo() {
-    document.querySelector("#home").hidden = true;
-    document.querySelector("#quiz").hidden = true;
-    document.querySelector("#post-game-input").hidden = true;
-    document.querySelector("#score-page").hidden = false;
-    document.querySelector("#home-button").hidden = false;
-    document.querySelector("#score-page-button").hidden = true;
-};
 
 var questionArray = [
     {
@@ -82,15 +64,52 @@ var questionArray = [
     }
 ];
 
-var currentQuestion = [];
-populateQuestion();
+
+
+var answerButtonAll = document.querySelectorAll(".answerButton");
+    for (x = 0; x <answerButtonAll.length; x++){
+        answerButtonAll[x].addEventListener("click", function(){
+            // if (answerButtonAll.innerHTML != questionArray.correctAnswer){
+            //     alert("working");
+            // }
+            questionIndex++;
+            populateQuestion();
+        });
+    }
+
+var questionIndex = 0;
+
 function populateQuestion() {
-    currentQuestion = questionArray[0];
-    console.log(currentQuestion);
+    if (questionIndex >= questionArray.length) {scoreInput();}
+        
+    
+     currentQuestion = questionArray[questionIndex];
+     question.textContent = currentQuestion.questionText;
+     aButton1.textContent = currentQuestion.answers[0]
+     aButton2.textContent = currentQuestion.answers[1]
+     aButton3.textContent = currentQuestion.answers[2]
+     aButton4.textContent = currentQuestion.answers[3]
+        // console.log(answerButtonAll);
+        // console.log(questionArray.toString);
+        // console.log(currentQuestion);
+     };
+     
 
-  //questionIndex++;
+var score = "";
 
+var timeLeft = 61;
+
+function startClock(){
+    var clockInterval = setInterval(function(){
+        timeLeft--;
+        timer.textContent = timeLeft;
+        if(timeLeft === 0) {
+            clearInterval(clockInterval);
+            scorePageGo();
+        }
+    }, 1000);
 }
+
 
 function startQuiz() {
     document.querySelector("#home").hidden = true;
@@ -99,7 +118,46 @@ function startQuiz() {
     document.querySelector("#score-page").hidden = true;
     document.querySelector("#home-button").hidden = true;
     document.querySelector("#score-page-button").hidden = false;
-
-    
+    startClock();
+    populateQuestion();
 }
 
+pageLoad();
+
+function clearScores() {};
+
+function pageLoad() {
+    document.querySelector("#home").hidden = false;
+    document.querySelector("#quiz").hidden = true;
+    document.querySelector("#post-game-input").hidden = true;
+    document.querySelector("#score-page").hidden = true;
+    document.querySelector("#home-button").hidden = true;
+    document.querySelector("#score-page-button").hidden = false;
+    if (questionIndex >= questionArray.length) {
+        window.location.reload();
+    }
+};
+
+function scoreInput() {
+    document.querySelector("#home").hidden = true;
+    document.querySelector("#quiz").hidden = true;
+    document.querySelector("#post-game-input").hidden = false;
+    document.querySelector("#score-page").hidden = true;
+    document.querySelector("#home-button").hidden = true;
+    document.querySelector("#score-page-button").hidden = true;
+    timer.hidden = true;
+};
+
+function scorePageGo() {
+    homePage.hidden = true;
+    quizPage.hidden = true;
+    initialsPage.hidden = true;
+    scorePage.hidden = false;
+    homeButton.hidden = false;
+    scorePageButton.hidden = false;
+    timer.hidden = true;
+};
+
+function scoreStorage() {
+
+}
