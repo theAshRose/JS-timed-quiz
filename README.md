170cea44385c99b8dcdcd4a105e82623c3fad21a
theDomconrad <them.and.us.2013@gmail.com>
2022-10-03 17:50:45 -0700

    updated readME, fixed ALL BUGS, finished styling, added notes to JS file

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # JS-timed-quiz
+
+## Description
+
+This is my second iteration of using JavaScript logic. This application was created by myself from scratch. [Here is a link to the application](https://thedomconrad.github.io/JS-timed-quiz/)
+I practiced using web APIs to execute the following for this quiz:
+--Show and hide the page depending on where the user needs to interact. 
+--Run a timer - which reduces by 15 seconds for every missed question and ends the quiz if time runs out.
+--Display wether or not the user selected the correct answer - as well as keep track of their score.
+--Take the user's initials at the end and compile it with their score on a score sheet - which the user may clear if they wish.
+
+I am proud of this project as it took a lot of energy to learn how to implement the technologies used. Even the CSS - the positioning of every element on the page - demonstrates knowledge I have learned practicing web development thus far.
+
+## How to use
+
+Simply click the start button and follow the prompts! Easy to use and difficult to master ;)
+
+## Screenshot of app
+
+![screenshot](./assets/images/js-timed-quiz.png)
+
+## the Code!
+Here I am showing the conditionals placed on the questions to verify if it was correct or not. This was the most important piece as it:
+--Populates the next question
+--Increases score OR reduces time
+--Sends a message to the user if their answer was right or wrong
+
+```
+var answerButtonAll = document.querySelectorAll(".answerButton");
+    for (x = 0; x <answerButtonAll.length; x++){
+        answerButtonAll[x].addEventListener("click", function(event){
+             if (event.target.innerText !== questionArray[questionIndex].correctAnswer) {
+                timeLeft = timeLeft - 15;
+                answerStatus.textContent = "WRONG ANSWER! NO SOUP FOR YOU!";
+             }
+             if (event.target.innerText == questionArray[questionIndex].correctAnswer) {
+                userScore = userScore +3;
+                answerStatus.textContent = "Correct! Cake at the end!";
+             }
+            questionIndex++
+            populateQuestion()
+        });
+    }
+
+```
+
+## Author Links
+---[Linkedin](https://www.linkedin.com/in/dominic-conradson-76638b172/)---
+[GitHub](https://github.com/theDomConrad/)---
+[Portfolio](https://thedomconrad.github.io/Dominic-Conradson-Portfolio/)---
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//basic variables definition
 var homeButton = document.querySelector("#home-button");
 var clearScore = document.querySelector("#clear-button");
 var scoreList = document.querySelector("#score-list");
@@ -14,23 +15,20 @@ var quizPage = document.querySelector("#quiz");
 var timer = document.querySelector("#timer");
 var initialsPage = document.querySelector("#post-game-input");
 
-
-
+//global event listeners, these mostly initiate page switching
 startButton.addEventListener("click", startQuiz);
 scorePageButton.addEventListener("click", scorePageGo);
 clearScore.addEventListener("click", clearScores);
-homeButton.addEventListener("click", pageLoad);
+homeButton.addEventListener("click", pageLoad2);
 
-/// -----------------------------
-
+//this app is using .hidden to switch between sections currently in action. so each page display setup is nested in a function
 var initialsButton = document.querySelector("#user-initials-button");
 var initialsInput = document.querySelector("#user-initials");
 var initialScore = document.querySelector("#user-score");
 
-
-
+var userScore = 0;
+//this app is using .hidden to switch between sections currently in action. so each page display setup is nested in a function
 function scorePageGo() {
-    scoreList.hidden = false;
     homePage.hidden = true;
     quizPage.hidden = true;
     initialsPage.hidden = true;
@@ -38,10 +36,16 @@ function scorePageGo() {
     homeButton.hidden = false;
     scorePageButton.hidden = true;
     timer.hidden = true;
+//this renders the list on the scorepage, it only creates a list if there is local storage of data. 
+var highScores = JSON.parse(localStorage.getItem("scoreString")) || [];
+    highScores.forEach(function(score){
+        var liElem = document.createElement("li");
+        liElem.textContent = score.initials + " -- " + score.score;
+        scoreList.appendChild(liElem);
 
-
+    });
 };
-
+//this is when the user inputs their initials at the end, the score+user input is made into an array of objects, then put into a string before being stored w/ JSON
 initialsButton.addEventListener("click", function(event) {
     event.preventDefault();
     var highScores = JSON.parse(localStorage.getItem("scoreString")) || [];
@@ -50,31 +54,17 @@ initialsButton.addEventListener("click", function(event) {
         score: userScore,
         initials: initials,
     }
-    window.localStorage.clear(highScores)
-    highScores.push(scoreString)
+    window.localStorage.clear(highScores);
+    highScores.push(scoreString);
     localStorage.setItem("scoreString", JSON.stringify(highScores));
-    renderScores();
     scorePageGo();
 });
-
-function renderScores() {
-    var highScores = JSON.parse(localStorage.getItem("scoreString")) || [];
-    highScores.forEach(function(score){
-        var liElem = document.createElement("li");
-        liElem.textContent = score.initials + " -- " + score.score;
-        scoreList.appendChild(liElem);
-
-    });
-}
-
+//this simply clears local storage and hides our current list, when the page reloads on the home button activation, a list doesnt exist because its not populated by our function in scorepagego function.
 function clearScores() {
     scoreList.hidden = true;
     window.localStorage.clear();
-    for (x = 0; x < scoreList.length; x++) {
-        scoreList[x].removeChild(liElem)
-    };
 };
-
+//an array of objects to populate our questions
 var questionArray = [
     {
         questionText: "Fill in blank: JavaScript is a ______ language", 
@@ -104,7 +94,7 @@ var questionArray = [
     {
         questionText: "which value does an 'if' statement return?", 
         answers: ["string", "intiger", "naan", "truthy/falsey"],
-        correctAnswer: "thruthy/falsey",
+        correctAnswer: "truthy/falsey",
     },
     {
         questionText: "how do we retrieve an ID'd with queryselector?", 
@@ -127,7 +117,7 @@ var questionArray = [
         correctAnswer: "document.write()",
     }
 ];
-
+//answer buttons all get selected, then iterate through a for loop and measure current question selected from array. if statements follow up with either time reduction or score increase, a written note to user about whether the question was correct or not, and then they trigger population of next question
 var answerButtonAll = document.querySelectorAll(".answerButton");
     for (x = 0; x <answerButtonAll.length; x++){
         answerButtonAll[x].addEventListener("click", function(event){
@@ -140,15 +130,13 @@ var answerButtonAll = document.querySelectorAll(".answerButton");
                 answerStatus.textContent = "Correct! Cake at the end!";
              }
             questionIndex++
-            populateQuestion();
+            populateQuestion()
         });
     }
-
+//below function populates our question, and if we run outof questions the test is ended
 var questionIndex = 0;
-
 function populateQuestion() {
     if (questionIndex >= questionArray.length) {scoreInput();}
-     console.log(questionIndex);
      currentQuestion = questionArray[questionIndex];
      question.textContent = currentQuestion.questionText;
      aButton1.textContent = currentQuestion.answers[0]
@@ -158,20 +146,19 @@ function populateQuestion() {
      };
      
 var timeLeft = 61;
-
+//it is what is sounds like, there is a conditional for when the timer runs out to end the test
 function startClock(){
     var clockInterval = setInterval(function(){
         timeLeft--;
         timer.textContent = timeLeft;
-        if(questionIndex >= questionArray.length|| timeLeft === 0 || timeLeft < 0 ) {
+        if(questionIndex >= questionArray.length|| timeLeft <= 0 ) {
             clearInterval(clockInterval);
             scoreInput();
         }
     }, 1000);
 }
-
+//in this function we show the test section, hide the rest and run our clock and populatequestion functions
 function startQuiz() {
-    timer.hidden = false;
     homePage.hidden = true;
     quizPage.hidden = false;
     initialsPage.hidden = true;
@@ -181,24 +168,26 @@ function startQuiz() {
     startClock();
     populateQuestion();
 }
-
+//this is called upon on page load to show our start screen and hide everything else
 function pageLoad() {
-    timeLeft = 60;
-    questionIndex = 0;
-    userScore = 0;
     homePage.hidden = false;
     quizPage.hidden = true;
     initialsPage.hidden = true;
     scorePage.hidden = true;
     homeButton.hidden = true;
     scorePageButton.hidden = false;
-     if (timeLeft < 0) {
-         window.location.reload()
-     }
-};
-
-var userScore = 0;
-
+}
+//this is a function i had to make which is specifically tied to the "return home" button at the end of the quiz. this refreshes the page and gets rid of lingering high score lists which would otherwise persist
+function pageLoad2() {
+    homePage.hidden = false;
+    quizPage.hidden = true;
+    initialsPage.hidden = true;
+    scorePage.hidden = true;
+    homeButton.hidden = true;
+    scorePageButton.hidden = false;
+    window.location.reload();
+}
+//this function is called at the end of the quiz from different functions for different reasons
 function scoreInput() {
     initialScore.textContent = "You're score: " + userScore;
     homePage.hidden = true;
@@ -212,5 +201,3 @@ function scoreInput() {
 };
 
 pageLoad();
-
-
